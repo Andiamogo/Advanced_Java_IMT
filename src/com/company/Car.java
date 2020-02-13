773324a5321ec47a6bfc8889aa8ed596f2cc1f8a
Maxime Perrier <50055110+Andiamogo@users.noreply.github.com>
2020-02-13 15:47:56 +0100

    Check if othe car is allowed to move to a certain point
    yes

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -6,23 +6,48 @@ import java.awt.*;
 import java.util.List;
 
 public class Car {
-    Point2D point2D;
+    Point2D position;
+    Point2D previous_movement;
+    int amount_movements = 0;
 
     public Car(int x, int y){
-        this.point2D = new Point2D(x, y);
+        this.position = new Point2D(x, y);
+        this.previous_movement = new Point2D(0, 0);
     }
 
     public int getX() {
-        return this.point2D.x;
+        return this.position.x;
     }
 
     public int getY() {
-        return this.point2D.y;
+        return this.position.y;
     }
 
-    void move(int x, int y){
-        int previous_x = getX();
-        int previous_y = getY();
-        this.point2D = new Point2D(previous_x + x, previous_y + y);
+    public int getAmount_movements() {
+        return this.amount_movements;
+    }
+
+    public void move(int x, int y){
+
+        if(this.amount_movements != 0){
+
+            int expected_x = this.previous_movement.x + this.position.x;
+            int expected_y = this.previous_movement.y + this.position.y;
+
+            if(((x - expected_x <= 1) && (x - expected_x >= -1)) && ((y - expected_y <= 1) && (y - expected_y >= -1))){
+                moveDaCar(x, y);
+                System.out.println("Déplacement OK");
+            }
+            else System.out.println("Déplacement NOK");
+        }
+        else{
+            moveDaCar(x, y);
+        }
+    }
+
+    private void moveDaCar(int x, int y){
+        this.amount_movements++;
+        this.previous_movement = new Point2D(x - this.position.x, y - this.position.y);
+        this.position = new Point2D(x, y);
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,8 +6,15 @@ import org.junit.jupiter.api.Test;
 public class Main {
 
     public static void main(String[] args) {
-        Car lambo = new Car(-2,-6);
-        lambo.move(3,4);
-        System.out.println("x : " + lambo.getX() + " ||| y : " + lambo.getY());
+        Car lambo = new Car(0,0);
+        System.out.println("x : " + lambo.getX() + " ||| y : " + lambo.getY() + " ||| movement : " + lambo.getAmount_movements());
+        lambo.move(2,2);
+        System.out.println("x : " + lambo.getX() + " ||| y : " + lambo.getY() + " ||| movement : " + lambo.getAmount_movements());
+        lambo.move(3,5);
+        System.out.println("x : " + lambo.getX() + " ||| y : " + lambo.getY() + " ||| movement : " + lambo.getAmount_movements());
+        lambo.move(4,8);
+        System.out.println("x : " + lambo.getX() + " ||| y : " + lambo.getY() + " ||| movement : " + lambo.getAmount_movements());
+        lambo.move(6,12);
+        System.out.println("x : " + lambo.getX() + " ||| y : " + lambo.getY() + " ||| movement : " + lambo.getAmount_movements());
     }
 }
